af5f7c07972e6eb0b8165d387cb9330b8f5cfbd0
alshaimaa <alshaimaaehab25@gmail.com>
2024-10-31 13:57:34 +0200

    very important update to my code !!! alshaimaa

--- Assets/scripts/githup.cs
+++ Assets/scripts/githup.cs
@@ -7,7 +7,7 @@ public class githup : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("hello samiha");
     }
 
     // Update is called once per frame
